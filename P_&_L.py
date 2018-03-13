6dd00d5515419e3745c7e4ec7b647ec6cddb2637
Rodrigo Muñoz Muñoz <rodrigo.mcuadrada@gmail.com>
2018-03-13 02:34:12 -0600

    starting

---
+++ P_&_L.py
@@ -0,0 +1,12 @@
+"""Imports"""
+import IEM as iem
+import pickle as pkl
+import random
+
+"""Variables and functions"""
+cls = iem.cls()
+pause = iem.pause()
+
+
+
+"""Main"""
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # P and L
+
+Welcome to Itspaper´s Produces and losses.
+
+This program will help you keep track of your expenses and earnings on a daily basis.
