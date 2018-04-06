9503ce5e94d84de830ba1bed6300fbae1fddba02
Rodrigo Muñoz Muñoz <rodrigo.mcuadrada@gmail.com>
2018-04-06 01:25:31 -0500

    Updates...

--- P_and_L.py
+++ P_and_L.py
@@ -2,11 +2,13 @@
 import IEM as iem
 import pickle as pkl
 import random
+import datetime as dt
 
 """Variables and functions"""
-cls = iem.cls()
-pause = iem.pause()
-
+pause = iem.pause
+cls = iem.cls
+x = dt.date(2018,4,5)
 
 
 """Main"""
+print(x)
